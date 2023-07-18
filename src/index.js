1dbf591423ebf25a8189794b96b676471baa47aa
Seb <valdez.sebastian4@gmail.com>
2023-07-17 19:32:15 -0700

    Add Rsvp page

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
+import Header from './components/Header';
 import App from './App';
 import {FourOhFour} from './pages/FourOhFour';
 import {Travel} from './pages/Travel'
 import {WeddingRegistry} from './pages/WeddingRegistry'
+import {Rsvp} from './pages/Rsvp'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header/>
       <Routes>
         <Route path='/' element={<App/>} />
         <Route path='/travel' element={<Travel/>} />
         <Route path='/registry' element={<WeddingRegistry/>} />
+        <Route path='/rsvp' element={<Rsvp/>} />
+
         <Route path='*' element={<FourOhFour />} />
       </Routes>
     </Router>
---
+++ src/pages/Rsvp.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export const Rsvp = () => {
+  return (
+    <div className='App-header'>
+      <h2>Rsvp Page</h2>
+      <Link to='/'>Back</Link>
+    </div>
+  )
+}
