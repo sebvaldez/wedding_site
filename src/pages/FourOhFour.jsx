7b34fa958d33c1b0f922b8a9df0d1e1f0ca9f1e3
Seb <valdez.sebastian4@gmail.com>
2023-07-18 23:27:11 -0700

    404 gifs

--- src/pages/FourOhFour.jsx
+++ src/pages/FourOhFour.jsx
@@ -1,13 +1,29 @@
-import { Link } from 'react-router-dom'
-import Container from '../components/Container'
+import { Link } from 'react-router-dom';
+import Container from '../components/Container';
+
+const timRobinsonGifs = [
+  'https://media.giphy.com/media/bjB3gtFvREqqr5NAHW/giphy.gif',
+  'https://media.giphy.com/media/J9MnwDiwCmjkexy9e1/giphy.gif',
+  'https://media.giphy.com/media/CEPCCpYP8baFh7ISQO/giphy.gif',
+  'https://media.giphy.com/media/GHdE1VNN1KknPeZqoQ/giphy.gif',
+  'https://media.giphy.com/media/6cFYYRYXCmS3hDAuXv/giphy.gif',
+];
+
+const getRandomGif = () => {
+  const randomIndex = Math.floor(Math.random() * timRobinsonGifs.length);
+  return timRobinsonGifs[randomIndex];
+}
 
 export const FourOhFour = () => {
   return (
     <Container md>
-      <section style={{ height: '350px' }}>
+      <section style={{ height: '550px' }}>
         <h1>404</h1>
         <h2>Page not found.</h2>
         <Link to='/'>Back</Link>
+        <br />
+        <img src={getRandomGif()} alt="Tim Robinson Gif" />
+        <br />
       </section>
     </Container>
   )
