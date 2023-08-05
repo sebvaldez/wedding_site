d694d237f2f877f1b01fb5f1550368a303608fb6
Seb <valdez.sebastian4@gmail.com>
2023-08-05 14:42:20 -0700

    Modal and image are more responsive

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,15 +5,26 @@ import styled from 'styled-components';
 import ModalPortal from './ModalPortal';
 
 const StyledModal = styled.div`
+  width: 85%; /* Default to mobile */
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   top: 50%;
   left: 50%;
-  width: 100%;
   transform: translate(-50%, -50%);
   z-index: 1000;
   background: white;
   padding: 20px;
   border-radius: 8px;
+
+  @media (min-width: 769px) {
+    width: 70%; /* Tablet */
+  }
+
+  @media (min-width: 1025px) {
+    width: 40%; /* Desktop */
+  }
 `;
 
 const ModalBackdrop = styled.div`
@@ -25,29 +36,6 @@ const ModalBackdrop = styled.div`
   background: rgba(0, 0, 0, 0.5);
   z-index: 900;
 `;
-
-// const StyledBackdrop = styled.div`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   background: rgba(0, 0, 0, 0.7);
-//   z-index: 999;
-// `;
-
-// const StyledModal = styled(animated.div)`
-//   position: fixed;
-//   top: 50%;
-//   left: 50%;
-//   width: 100%;
-//   transform: translate(-50%, -50%);
-//   z-index: 1000;
-//   background: white;
-//   padding: 20px;
-//   border-radius: 8px;
-//   overflow-y: auto;
-// `;
 
 const Modal = ({ onClose, children }) => (
   <ModalPortal>
--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledResponsiveImage = styled.img`
-  width: 100%;
+  width: 80%;
   height: auto;
   display: block;  // To remove any spacing below the image
   margin: 1rem 0;  // Add some spacing above and below the image
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
-const ResponsiveImage = ({ children }) => {
-  return <StyledResponsiveImage>{children}</StyledResponsiveImage>
+const ResponsiveImage = ({ src, alt, children }) => {
+  return <StyledResponsiveImage src={src} alt={alt}>{children}</StyledResponsiveImage>
 }
 
 export default ResponsiveImage;
