b845bd732e26a104b72ca5c08787833558a6c881
Seb <valdez.sebastian4@gmail.com>
2023-08-05 12:09:11 -0700

    Fix redirect to login, fix protect route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    // Optionally, return a loading spinner or message
+    return <div>Loading...</div>;
+  }
 
   if (isAuthenticated) {
     return children;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,130 +1,27 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-import styled from 'styled-components';
-
-const LoginContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 50px;
-    border-radius: 5px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    max-width: 800px;
-    margin: auto;
-    margin-top: 50px;
-`;
-
-const Heading = styled.h2`
-    margin-bottom: 20px;
-    font-size: 24px;
-    font-weight: bold;
-`;
-
-const InputGroup = styled.div`
-    width: 100%;
-    margin-bottom: 20px;
-`;
-
-const Label = styled.label`
-    display: block;
-    margin-bottom: 5px;
-    font-weight: 600;
-`;
-
-const Input = styled.input`
-    width: 100%;
-    padding: 10px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    font-size: 16px;
-    transition: border-color 0.3s ease;
-
-    &:focus {
-        border-color: #007BFF;
-        outline: none;
-    }
-`;
-
-const LoginButton = styled.button`
-    padding: 10px 20px;
-    background-color: #007BFF;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 18px;
-    transition: background-color 0.3s ease;
-
-    &:hover {
-        background-color: #0056b3;
-    }
-`;
+import { Navigate } from "react-router-dom";
 
 const Admin = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
-  // const [email, setEmail] = useState('');
-  // const [passwordx , setPassword] = useState('');
-  // const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [loginWithRedirect, isAuthenticated, isLoading]);
 
-  // const handleLogin = (e) => {
-  //   e.preventDefault();
+  if (isAuthenticated) {
+    // Redirect to the dashboard if the user is authenticated
+    return <Navigate to="/dashboard" replace />;
+  }
 
-  //   const isAuthenticated = mockApiCall(email, password);
-
-  //   if (isAuthenticated) {
-  //     navigate('/dashboard');
-  //   } else {
-  //     alert('Invalid credentials. Please try again.');
-  //   }
-  // };
-
-  // const mockApiCall = (email, password) => {
-  //   const validEmails = [
-  //     'valdez.sebastian@gmail.com',
-  //     'allegracesena@gmail.com',
-  //     'dev@example.com'
-  //   ];
-
-  //   if (validEmails.includes(email) && password === 'admin') {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // };
-
-  // return (
-  //   <>
-  //     <LoginContainer>
-  //       <Heading>Admin Login</Heading>
-  //       <form onSubmit={handleLogin}>
-  //         <InputGroup>
-  //           <Label>Email:</Label>
-  //           <Input
-  //             type="email"
-  //             value={email}
-  //             onChange={(e) => setEmail(e.target.value)}
-  //             required
-  //           />
-  //         </InputGroup>
-
-  //         <InputGroup>
-  //           <Label>Password:</Label>
-  //           <Input
-  //             type="password"
-  //             value={password}
-  //             onChange={(e) => setPassword(e.target.value)}
-  //             required
-  //           />
-  //         </InputGroup>
-
-  //         <LoginButton type="submit">Login</LoginButton>
-  //       </form>
-  //     </LoginContainer>
-  //   </>
-  // );
+  return (
+    <div>
+      {/* Conditional rendering based on isLoading */}
+      {isLoading ? 'Loading authentication state...' : 'Redirecting to login...'}
+    </div>
+  );
 };
 
 export default Admin;
