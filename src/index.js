9c806ec3cf18f4c865c39f6e87c7f0e40e674916
Seb <valdez.sebastian4@gmail.com>
2023-08-07 23:44:03 -0700

    Install @tanstack

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GlobalFonts, GlobalStyle } from './styles';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,49 +11,50 @@ import App from './App';
 import Admin from './pages/Admin';
 import { FourOhFour } from './pages/FourOhFour';
 import { Gallery } from './pages/Gallery';
-import { Travel } from './pages/Travel'
-import { WeddingRegistry } from './pages/WeddingRegistry'
-import { Rsvp } from './pages/Rsvp'
-import ProtectedRoute from './components/ProtectedRoute'
+import { Travel } from './pages/Travel';
+import { WeddingRegistry } from './pages/WeddingRegistry';
+import { Rsvp } from './pages/Rsvp';
+import ProtectedRoute from './components/ProtectedRoute';
 import reportWebVitals from './reportWebVitals';
 import AdminDashboard from './pages/AdminDashboard';
 
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: `${window.location.origin}/dashboard`
-      }}
-    >
-      <GlobalStyle />
-      <GlobalFonts />
-      <Router>
-        <Header/>
+    <QueryClientProvider client={queryClient}>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: `${window.location.origin}/dashboard`
+        }}
+      >
+        <GlobalStyle />
+        <GlobalFonts />
+        <Router>
+          <Header />
           <Routes>
-            <Route path='/' element={<App/>} />
-            <Route path='/admin' element={<Admin/>} />
+            <Route path='/' element={<App />} />
+            <Route path='/admin' element={<Admin />} />
             <Route path='/dashboard' element={
               <ProtectedRoute>
-                <AdminDashboard/>
+                <AdminDashboard />
               </ProtectedRoute>
             } />
-            <Route path='/gallery' element={<Gallery/>} />
-            <Route path='/travel' element={<Travel/>} />
-            <Route path='/registry' element={<WeddingRegistry/>} />
-            <Route path='/rsvp' element={<Rsvp/>} />
+            <Route path='/gallery' element={<Gallery />} />
+            <Route path='/travel' element={<Travel />} />
+            <Route path='/registry' element={<WeddingRegistry />} />
+            <Route path='/rsvp' element={<Rsvp />} />
             <Route path='*' element={<FourOhFour />} />
           </Routes>
-        <Footer />
-      </Router>
-    </Auth0Provider>
+          <Footer />
+        </Router>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Auth0Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
