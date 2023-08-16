c645afc0a4d1bc164b5540f24e422613f42810cd
Seb <valdez.sebastian4@gmail.com>
2023-08-15 22:59:02 -0700

    RSVP working, mobile styles for 404

--- src/pages/FourOhFour.jsx
+++ src/pages/FourOhFour.jsx
@@ -1,5 +1,7 @@
-import { Link } from 'react-router-dom';
-import Container from '../components/common/Container';
+import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHammer } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
 const timRobinsonGifs = [
   'https://media.giphy.com/media/bjB3gtFvREqqr5NAHW/giphy.gif',
@@ -14,17 +16,69 @@ const getRandomGif = () => {
   return timRobinsonGifs[randomIndex];
 }
 
+const FourOhFourSection = styled.section`
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1, h2 {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 20px 0;
+
+    h1 {
+      font-size: 32px;
+    }
+
+    h2 {
+      font-size: 24px;
+    }
+  }
+`;
+
+const BackLink = styled(Link)`
+  margin: 20px 0;
+  font-size: 18px;
+  padding: 10px 15px;
+  text-decoration: none; // if you want to remove the underline
+  color: inherit; // if you want the link color to be same as text
+`;
+
+const ResponsiveGif = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
 export const FourOhFour = () => {
+  const location = useLocation();
+
+  const displayRsvpMessage = location.pathname === '/rsvp';
+
   return (
-    <Container md>
-      <section style={{ height: '550px' }}>
-        <h1>404</h1>
-        <h2>Page not found.</h2>
-        <Link to='/'>Back</Link>
-        <br />
-        <img src={getRandomGif()} alt="Tim Robinson Gif" />
-        <br />
-      </section>
-    </Container>
+
+      <FourOhFourSection>
+        {displayRsvpMessage ? (
+          <div style={{ height: '60vh', display: 'flex', justifyContent: 'center', flexDirection: 'column', gap: '4rem'}}>
+            <h2>RSVP Page</h2>
+            <p>Coming soon! We're working on it.</p>
+            <FontAwesomeIcon icon={faHammer} size='4x' />
+          </div>
+        ) : (
+          <>
+            <h1>404</h1>
+            <h2>Page not found.</h2>
+            <ResponsiveGif src={getRandomGif()} alt="Tim Robinson Gif" />
+          </>
+        )}
+        <BackLink to='/'>Back to Home</BackLink>
+      </FourOhFourSection>
+
   )
-}
+};
