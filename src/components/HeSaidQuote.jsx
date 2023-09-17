f14f5fef4ed3fe92548aa65884d07cae42e54143
Seb <valdez.sebastian4@gmail.com>
2023-09-16 17:47:40 -0700

    Edits to HeSaid

--- src/components/HeSaidQuote.jsx
+++ src/components/HeSaidQuote.jsx
@@ -13,36 +13,32 @@ const StyledQuote = styled.blockquote`
   margin: 0 auto;  // centers the container on larger screens
 `;
 
+
 const HeSaidQuote = () => {
   return (
     <>
       <h2><em>He said...</em></h2>
 
       <StyledQuote>
-        Allegra and I have always stayed in touch after moving out of Lodi, where we grew up.
+        Allegra and I always stayed in touch after moving out of our hometown. Our shared sense of independence and curiosity created an instinctual sense of respect, comfort and trust with one another.
 
         <br /><br />
-        We are both independent and curious people in nature, shared qualities that created an instinctual sense of respect, comfort and trust.
+        Over the years, we found that during moments of significant life changes, we reached out to each other for advice and perspective.
         <br /><br />
 
-        During moments of significant change, whether it was moving, leaving a job, or needing another’s perspective on life,
-        we reached out to each other. This kind of friendship grew into a relationship once she moved to the Bay Area. During this time we had some of the most memorable laughs and conversations while exploring great coffee and food.
+        When I left my first adult job, she helped me write my resignation letter. When she wanted to move to the Bay Area, I was encouraging her to make a change. When she did,
+        our friendship grew into a relationship, and it was here that we had some of the most memorable laughs and conversations while exploring great coffee and food.
         <br /><br />
 
-        Having Allegra in my life is like having a secret power up! Whether they lasts months or hours,
-        she takes an interest in my hobbies and work, sharing my enthusiasm each step of the way.
-        Experiencing this in a relationship is something I&apos;ve not had before.
-        I love the particular ways in which she keeps me organized (even with my doom piles, haha sorry babe).
+        Having Allegra in my life is like having a secret power up! In my many hobbies and work projects, no matter how long they last,
+        she is always interested and encouraging. I love that we share enthusiasm for each other’s uniqueness, and the particular ways in which she helps me remain organized (even with my doom piles, haha sorry babe).
         <br /><br />
 
-        As the moment approached to shop for a ring, I felt an immense pressure to find the perfect one,
-        ensuring every detail and 'tech spec' was just right. Then, during one of my online searches,
-        I stumbled upon <strong>THE ONE</strong>. Even though it was just a image, I needed to hold it immediately.
-        That ring transported me into our future, I saw our life together as I was flooded with excitement,
-        joy and love for Allegra with the hopes of asking her to marry me.
+        So, as the moment approached to shop for a ring, I felt an immense pressure to find the perfect ring, ensuring every detail and 'tech spec' was just right.
+        Then, during one of my online searches, I stumbled upon THE ONE. I knew I needed to hold it. That ring transported me into our future, I saw our life together. I was flooded with excitement, joy, and love for Allegra with the hopes of asking her to marry me.
         <br /><br />
 
-        I am incredibly thankful to have her as my future wife and continuing to reciprocate that same energy she puts in.
+        I am incredibly thankful to have her as my future wife and look forward to continuing to reciprocate the love, care, and energy she brings.
         <br /><br />
       </StyledQuote>
     </>
