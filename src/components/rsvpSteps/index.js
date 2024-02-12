d0b223cd9fe4312d149c4e24b98fc7104bb529c1
Seb <valdez.sebastian4@gmail.com>
2024-02-11 18:04:37 -0800

    RSVP working for single member flow, talks with backend

--- src/components/rsvpSteps/index.js
+++ src/components/rsvpSteps/index.js
@@ -1,11 +1,13 @@
 import { AttendanceStep } from './AttendanceStep';
 import { ConfirmationStep } from './ConfirmationStep';
+import { ConfirmedStep } from './ConfirmedStep';
 import { EmailLookupStep } from './EmailLookupStep';
 import { GuestSelectionStep } from './GuestSelectionStep';
 
 export {
   AttendanceStep,
   ConfirmationStep,
+  ConfirmedStep,
   EmailLookupStep,
   GuestSelectionStep
 };
