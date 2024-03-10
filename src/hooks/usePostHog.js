8d25938d73811b69ce00b5eb14dfe922396c53b6
Seb <valdez.sebastian4@gmail.com>
2024-03-10 14:23:39 -0700

    Capture when in prod env

--- src/hooks/usePostHog.js
+++ src/hooks/usePostHog.js
@@ -3,7 +3,9 @@ import posthog from 'posthog-js';
 const usePosthog = () => {
 
   const trackEvent = (event, properties) => {
-    posthog.capture(event, properties);
+    if (process.env.REACT_APP_ENV === 'production') {
+      posthog.capture(event, properties);
+    }
   }
 
   return trackEvent
