fcb0ba5cc634cdcc0c856173a67187050a9d126d
Seb <valdez.sebastian4@gmail.com>
2024-03-13 22:45:09 -0700

    Create mock text fn

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,13 @@ export const toTitleCaseNoSpaces = (str) => {
     .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize the first letter of each word and make the rest lowercase
     .join(' '); // Join the words back together without spaces
 };
+
+export const mockSendText = ({ memberId, firstName }) => {
+  console.log(`Initiating sendText for ${firstName} (ID: ${memberId})`);
+
+  // Simulate a delay (e.g., network request) of 300 milliseconds
+  return new Promise(resolve => setTimeout(() => {
+    console.log(`Completed sendText for ${firstName} (ID: ${memberId})`);
+    resolve(`Message sent to ${firstName} (ID: ${memberId})`);
+  }, 300));
+};
