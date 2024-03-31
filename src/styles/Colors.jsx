97193d071fbe81a3aab8297df844e9592c7d4208
Seb <valdez.sebastian4@gmail.com>
2024-03-30 18:44:14 -0700

    New colors

--- src/styles/Colors.jsx
+++ src/styles/Colors.jsx
@@ -3,12 +3,18 @@
 // Dark Green - #414A43
 // Rust - #803404
 // Mauve - #995A6D
-// Burgundy - #520707
+// Burgundy - #52072D
+// Cream - #EDDCD2
+// Gold - #9B6F23
+// Cooper - #5D2D24
 
 export const COLOR_PALETTE = {
   sageGreen: '#9C9F7F',
+  cream: '#EDDCD2',
+  mauve: '#995A6D',
+  gold: '#9B6F23',
   darkGreen: '#414A43',
   rust: '#803404',
-  mauve: '#995A6D',
-  burgundy: '#520707'
+  burgundy: '#52072D',
+  cooper: '#5D2D24'
 }
