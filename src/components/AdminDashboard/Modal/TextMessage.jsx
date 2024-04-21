c26b6fa2eb0fa6a1af30ab1d4053cf24a0203045
Seb <valdez.sebastian4@gmail.com>
2024-04-21 14:32:17 -0700

    Add more templates

--- src/components/AdminDashboard/Modal/TextMessage.jsx
+++ src/components/AdminDashboard/Modal/TextMessage.jsx
@@ -64,6 +64,38 @@ const TextMessage = () => {
             Its that time to RSV, Please check your email for the link to RSVP. We look forward to seeing you soon!
           </blockquote>
         </div>
+        <div>
+          <input
+            type="radio"
+            id="template4"
+            name="textTemplate"
+            value="template4"
+            checked={selectedTemplate === 'template4'}
+            onChange={handleTemplateChange}
+          />
+          <label htmlFor="template4">
+            <strong>Template 4</strong>
+          </label>
+          <blockquote>
+            Its that time to RSV, Please check your email for the link to RSVP. We look forward to seeing you soon!
+          </blockquote>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="template5"
+            name="textTemplate"
+            value="template5"
+            checked={selectedTemplate === 'template5'}
+            onChange={handleTemplateChange}
+          />
+          <label htmlFor="template5">
+            <strong>Template 5</strong>
+          </label>
+          <blockquote>
+            Its that time to RSV, Please check your email for the link to RSVP. We look forward to seeing you soon!
+          </blockquote>
+        </div>
         <SubmitButton type="submit">
           Bulk send texts
           <FontAwesomeIcon icon={faPaperPlane} size='lg' style={{ marginLeft: '0.5rem' }} />
