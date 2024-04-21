a8e8895a1f7e041c866e0c0bd95c7feb5032dc50
Seb <valdez.sebastian4@gmail.com>
2024-04-21 14:31:59 -0700

    Style

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,13 +1,21 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 const StyledLogoutButton = styled.button`
   font-family: 'Montserrat', sans-serif;
-  font-size: 1em;
   color: #000000;
   text-decoration: none;
-  margin: 0 1rem;
+  padding: .5rem;
+  margin: 0rem;
+  font-size: 1.3rem;
+  font-weight: bold;
+
+  text-decoration: none;
+  /* margin: 0 .5rem; */
   border: none;
   background: transparent;
   cursor: pointer;
@@ -31,6 +39,7 @@ const LogoutButton = () => {
   return (
     <StyledLogoutButton onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Log Out
+      <FontAwesomeIcon icon={faArrowRight} size='lg' style={{ marginLeft: '0.5rem' }} />
     </StyledLogoutButton>
   );
 };
