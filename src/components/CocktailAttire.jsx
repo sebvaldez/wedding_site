93b809a9bf8bd07627e151de0c95e08a90d39432
Seb <valdez.sebastian4@gmail.com>
2024-05-13 21:39:51 -0700

    Add inspo link

--- src/components/CocktailAttire.jsx
+++ src/components/CocktailAttire.jsx
@@ -2,10 +2,14 @@ import  Section  from './layout/Section';
 
 export default function CocktailAttire() {
   return (
-      <Section title='Guest Attire'>
+      <Section style={{ gap: '.5rem'}} title='Guest Attire'>
         <p style={{ fontSize: '1.5rem'}}><em>Cocktail Formal</em></p>
         <p>Please refrain from wearing the following items:</p>
         <p>Denim, Shorts, Baseball Caps, Athletic Wear, Athletic Shoes.</p>
+
+        <a href="https://pin.it/1UjE8D8I9" _target='blank'>
+          <p style={{ paddingTop: '.2rem', fontWeight: '600'}}><em>Need inspiration? <u>CLICK HERE</u></em></p>
+        </a>
       </Section>
   )
 };
