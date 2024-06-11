788edf09a2f34f350d688138d9ead6e2c2a309bc
Seb <valdez.sebastian4@gmail.com>
2024-06-10 23:33:51 -0700

    Filter logic

--- src/components/AdminDashboard/Modal/EmailMessage.jsx
+++ src/components/AdminDashboard/Modal/EmailMessage.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
 import Modal from '../../common/Modal';
+import { LoadingEllipse } from '../../common/LoadingEllipse';
 import { SubmitButton } from '../../common/formStyles'
 
 const PromptContainer = styled.div`
@@ -13,16 +15,29 @@ const PromptContainer = styled.div`
   margin-top: 1rem;
 `;
 
-const EmailMessage = () => {
+
+const BAD_EMAIL = 'no-email@test.com';
+
+const filterMembers = members => members.filter(member => !!member.email && member.email !== BAD_EMAIL);
+
+const EmailMessage = ({ members, sendBulkEmails, isLoading}) => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validMembers = filterMembers(members);
+
+    sendBulkEmails({  members: validMembers });
+  }
+
   return <Modal.Content>
     <Modal.Header>Email All Guests</Modal.Header>
       <PromptContainer>
         <h1 style={{ padding: '5rem 0 6rem 0', fontSize: '2rem' }}>Send all guests the RSVPs?</h1>
       </PromptContainer>
-      <SubmitButton type="submit">
-        Send Email
-        <FontAwesomeIcon icon={faPaperPlane} size='lg' style={{ marginLeft: '0.5rem' }} />
-      </SubmitButton>
+        <SubmitButton onClick={handleSubmit} type="submit" disabled={isLoading}>
+          {isLoading ? 'Emailing members' : 'Bulk send emails'}
+          {isLoading ? <LoadingEllipse size={'sm'}color={'#fff'} />: <FontAwesomeIcon icon={faPaperPlane} size='lg' /> }
+        </SubmitButton>
   </Modal.Content>;
 }
 
